df7ca0c7cb0a95d1edb1d5ad9bf677f773501514
gugusd999 <49971455+gugusd999@users.noreply.github.com>
2020-04-17 16:18:58 +0700

    pembaruan database

--- js/page/siswa/kelasSiswa.js
+++ js/page/siswa/kelasSiswa.js
@@ -11,29 +11,61 @@ define(['jquery', 'main', 'helper', 'frb', '@firebase/app','@firebase/auth', '@f
 
         let html = ``;
 
-        value.val().forEach((item, i) => {
-          let {jenjang, kelas} = item;
-          html +=  `
-            <a href="#/siswa-kelas-soal/${jenjang}-${kelas}" class="col-sm-6  mt-3">
-              <div class="card">
-                <div class="card-body text-center" id="crud">
-                  <div class="row">
-                    <div class="col-sm-4">
-                      <img class="img-fluid img-menu" src="./asset/icon-siswa.png" alt="">
-                    </div>
-                    <div class="col-sm-8 text-left">
-                      <span style="font-size: 20px; font-weight: 700; color: black;">${jenjang}-${kelas}</span>
-                      <p class="text-dark"></p>
+        let dataku = value.val();
+
+        if (Array.isArray(dataku)) {
+          dataku.forEach((item, i) => {
+            let {jenjang, kelas} = item;
+            html +=  `
+              <a href="#/siswa-kelas-soal/${jenjang}-${kelas}" class="col-sm-6  mt-3">
+                <div class="card">
+                  <div class="card-body text-center" id="crud">
+                    <div class="row">
+                      <div class="col-sm-4">
+                        <img class="img-fluid img-menu" src="./asset/icon-siswa.png" alt="">
+                      </div>
+                      <div class="col-sm-8 text-left">
+                        <span style="font-size: 20px; font-weight: 700; color: black;">${jenjang}-${kelas}</span>
+                        <p class="text-dark"></p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </a>
-          `;
-        });
+              </a>
+            `;
+          });
 
 
-          $('body #syfaul-container').html(html);
+            $('body #syfaul-container').html(html);
+        }else{
+
+          let myKeys = Object.keys(dataku);
+
+          myKeys.forEach((item, i) => {
+            let {jenjang, kelas} = dataku[item];
+            html +=  `
+              <a href="#/siswa-kelas-soal/${jenjang}-${kelas}" class="col-sm-6  mt-3">
+                <div class="card">
+                  <div class="card-body text-center" id="crud">
+                    <div class="row">
+                      <div class="col-sm-4">
+                        <img class="img-fluid img-menu" src="./asset/icon-siswa.png" alt="">
+                      </div>
+                      <div class="col-sm-8 text-left">
+                        <span style="font-size: 20px; font-weight: 700; color: black;">${jenjang}-${kelas}</span>
+                        <p class="text-dark"></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            `;
+          });
+
+
+            $('body #syfaul-container').html(html);
+        }
+
 
       })
 
