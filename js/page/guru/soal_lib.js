594754e431ded47062f6710438bed6a0cc656154
gugusd999 <49971455+gugusd999@users.noreply.github.com>
2020-04-17 16:10:38 +0700

    pembaruan database

--- js/page/guru/soal_lib.js
+++ js/page/guru/soal_lib.js
@@ -12,7 +12,12 @@ define(['jquery', 'helper', 'frb', '@firebase/app', '@firebase/database'], funct
       let data = await firebase.database().ref(`guru/soal/${kelas}/${soal}/`+dataLogin.username+'/').once('value').then((value) => {
         if (value.val() != null) {
           let html = ``;
-          value.val().map((item, i) => {
+
+          let dataku = value.val();
+
+        if (Array.isArray(dataku)) {
+
+            dataku.map((item, i) => {
             let {soal, a, b, c, d, no, kunci, alasan} = item;
             return `
             <div class="col-sm-12 mt-3">
@@ -48,6 +53,54 @@ define(['jquery', 'helper', 'frb', '@firebase/app', '@firebase/database'], funct
             html += item;
           });
           $('body #syfaul-container').html(html);
+
+
+        }else{
+
+          let datakuS = Object.keys(dataku);
+
+
+          datakuS.map((item, i) => {
+            let {soal, a, b, c, d, no, kunci, alasan} = dataku[item];
+            return `
+            <div class="col-sm-12 mt-3">
+              <div class="card">
+                <div class="card-header">
+                  <span class="card-title">Soal No. ${no}</span>
+                </div>
+                <div class="card-body">
+                  <p>
+                    ${soal}
+                  </p>
+                  <p>
+                    pilihan :
+                  </p>
+                  <ul>
+                    <li>a) ${a}</li>
+                    <li>b) ${b}</li>
+                    <li>c) ${c}</li>
+                    <li>d) ${d}</li>
+                  </ul>
+                  <div>
+                    <span>Jawaban yang benar : ${kunci})</span>
+                  </div>
+                  <div>
+                    <span>Alasan: </span>
+                    <p>${alasan}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            `;
+          }).forEach((item) => {
+            html += item;
+          });
+          $('body #syfaul-container').html(html);
+
+
+        }
+
+          
 
         }
       });
